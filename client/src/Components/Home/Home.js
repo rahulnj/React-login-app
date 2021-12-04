2077a6c4523a1895870bf6cc004ed0e54ca14a97
Rahul <rahuljayaraman8@gmail.com>
2021-12-04 14:31:52 +0530

    logout added and admin changed

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
 import axios from "axios"
+import { Link, useNavigate } from "react-router-dom"
+
+
 
 import './Home.css';
 
 function Home() {
+
+    const navigate = useNavigate()
     const logoutHandler = async () => {
-
-
+        const data = await axios.get('/auth/logout')
+        console.log(data);
+        if (data) {
+            navigate('/login')
+        }
     }
 
     return (
